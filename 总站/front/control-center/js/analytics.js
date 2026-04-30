40b9fb104738f5f53b21cea617f147eebc24a1f5
Larquy <2644023588@qq.com>
2026-04-30 16:48:07 +0800

    Delete 总站 directory

--- 总站/front/control-center/js/analytics.js
+++
@@ -1,3 +0,0 @@
-// 设置日期
-const date = new Date();
-document.getElementById('dateInfo').textContent = date.toISOString().split('T')[0];
--- 总站/front/control-center/js/dispatch.js
+++
@@ -1,6 +0,0 @@
-// 实时更新时间
-setInterval(() => {
-    const date = new Date();
-    const timeString = date.toISOString().replace('T', ' ').substring(0, 19);
-    document.getElementById('dateInfo').textContent = timeString;
-}, 1000);
